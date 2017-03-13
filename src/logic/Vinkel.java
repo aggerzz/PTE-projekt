f94a648f41d74d548ccc3b6bdebe6d595eb46c48
Monster <bob4thegirl2014@gmail.com>
2017-03-13 14:01:51 +0100

    Implementation af OC 2 getVinkel

--- src/logic/Vinkel.java
+++ src/logic/Vinkel.java
@@ -1,8 +1,12 @@
 package logic;
 
 public interface Vinkel {
-	
 	public double getGrader();
-	
+
+	public void setGrader(double grader);
+
 	public boolean getMaaltTilLodret();
+
+	public void setMaaltTilLodret(boolean erMaaltTilLodret);
+
 }
--- src/logic/VinkelImpl.java
+++ src/logic/VinkelImpl.java
@@ -4,17 +4,36 @@ public class VinkelImpl implements Vinkel {
 
 	private double grader;
 	private boolean erMaaltTilLodret;
+	private double graderMaaltTilLodret;
+	private double graderMaaltTilVandret;
+	private String mellemregning;
 
 	@Override
 	public double getGrader() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (getMaaltTilLodret()) {
+			graderMaaltTilLodret = grader;
+			graderMaaltTilVandret = 90 - grader;
+
+			return graderMaaltTilVandret;
+		} else {
+			graderMaaltTilVandret = grader;
+			graderMaaltTilLodret = 90 - grader;
+
+			return graderMaaltTilLodret;
+		}
 	}
 
 	@Override
 	public boolean getMaaltTilLodret() {
-		// TODO Auto-generated method stub
-		return false;
+		return erMaaltTilLodret;
+	}
+	@Override
+	public void setMaaltTilLodret(boolean erMaaltTilLodret) {
+		this.erMaaltTilLodret = erMaaltTilLodret;
 	}
 
+	@Override
+	public void setGrader(double grader) {
+		this.grader = grader;
+	}
 }
