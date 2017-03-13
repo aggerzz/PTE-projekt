5a65abc1f06d1ae6f54bf2267a49bdb16fbf971b
Steffen <steffen_son@hotmail.com>
2017-03-13 15:09:26 +0100

    En del af OC4 implementeret samt ændring af beregning til mellemregning

--- src/logic/TvaerkraftImpl.java
+++ src/logic/TvaerkraftImpl.java
@@ -1,11 +1,13 @@
 package logic;
 
 public class TvaerkraftImpl implements Tvaerkraft {
-
+	private Vinkel vinkel;
+	private Dimensionerendekraft fdim;
+	private String mellemregning = null;
+	
 	@Override
 	public String getMellemregning() throws DimensionerendeKraftEjDefineretException, VinkelEjDefineretException {
-		// TODO Auto-generated method stub
-		return null;
+		return mellemregning;
 	}
 
 	@Override
@@ -16,14 +18,28 @@ public class TvaerkraftImpl implements Tvaerkraft {
 
 	@Override
 	public void angivVinkel(Vinkel vinkel) throws VinkelEjDefineretException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public double getNewton() throws DimensionerendeKraftEjDefineretException, VinkelEjDefineretException {
-		// TODO Auto-generated method stub
-		return 0;
+		double grader = vinkel.getGrader();
+		boolean erMaaltTilLodret = vinkel.getMaaltTilLodret();
+		double fdimNewton = fdim.getNewton();
+		double ftNewton = 0;
+		
+		if (erMaaltTilLodret) {
+			ftNewton = Math.cos(grader) * fdimNewton;
+			mellemregning = "FT = cos(vinkel) * FDim" + "\n" + ftNewton + " = cos(" + grader + ") * " + ftNewton; 
+		}
+		
+		else {
+			ftNewton = Math.sin(grader) * fdimNewton;
+			mellemregning = "FT = sin(vinkel) * FDim" + "\n" + ftNewton + " = sin(" + grader + ") * " + ftNewton; 
+		}
+		
+		return ftNewton;
 	}
 
 }
