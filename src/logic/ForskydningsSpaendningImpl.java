688c8594f047a293a3d23742feef52870b17971a
Nadatee <nadatee@hotmail.com>
2017-03-17 09:17:26 +0100

    Har implementeret ForskydningsSpaendning klasse

---
+++ src/logic/ForskydningsSpaendningImpl.java
@@ -0,0 +1,29 @@
+package logic;
+
+import exceptions.DimensionerendeKraftEjDefineretException;
+import exceptions.VinkelEjDefineretException;
+
+public class ForskydningsSpaendningImpl implements ForskydningsSpaendning {
+	private Areal a = new ArealImpl();
+	private Tvaerkraft ft = new TvaerkraftImpl();
+	private double mm2 = Double.NaN;
+	public double tau = Double.NaN;
+	private String mellemregning;	
+	
+	public double beregnTau() throws DimensionerendeKraftEjDefineretException, VinkelEjDefineretException{
+		double ftNewton = ft.getNewton();
+		
+		mm2 = a.getMm2();
+		tau = ftNewton / mm2;
+		setMellemregning("Tau = Ft / A = " + "\n" + ftNewton / mm2);
+		return tau ;		
+	}
+	
+	public void setMellemregning(String mellemregning){
+		this.mellemregning = mellemregning;
+	}
+	
+	public String getMellemregning(){
+		return mellemregning;
+	}	
+}
