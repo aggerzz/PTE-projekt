a08a267bb3476fe9173cb5bee6d7ae9e7280682b
Dan Toft <Tanddant@gmail.com>
2017-03-21 11:06:19 +0100

    Enheder virker i GUI

--- src/logic/Enhed.java
+++ src/logic/Enhed.java
@@ -1,8 +1,15 @@
 package logic;
 
 public enum Enhed {
-	GRAM,
-	KG,
-	TON,
-	NEWTON;
+	GRAM("g"),
+	KG("kg"),
+	TON("t"),
+	NEWTON("N");
+	private String enheder;
+	Enhed(String enheder){
+		this.enheder = enheder;
+	}
+	public String toString(){
+		return enheder;
+	}
 }
