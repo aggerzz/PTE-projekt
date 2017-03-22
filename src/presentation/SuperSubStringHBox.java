4e58a7b77370b0f170572ed5a596105bc85deb96
Monster <bob4thegirl2014@gmail.com>
2017-03-22 10:47:13 +0100

    Hævet skrift tilføjet og ændret

--- src/presentation/SuperSubStringHBox.java
+++ src/presentation/SuperSubStringHBox.java
@@ -9,15 +9,19 @@ public class SuperSubStringHBox {
 
 	public HBox generateSuperHBox(String firstString, String secondString) {
 		HBox hbox = new HBox();
+		HBox hbox2 = new HBox();
 		Label labelFirst = new Label();
 		Label labelSecond = new Label();
 		
 		labelFirst.setText(firstString);
 		labelSecond.setText(secondString);
-		labelSecond.setFont(new Font("Areal", 7));
-
-		hbox.setAlignment(Pos.TOP_LEFT);
-		hbox.getChildren().addAll(labelFirst, labelSecond);
+		labelSecond.setFont(new Font("Areal", 8));
+		
+		hbox2.getChildren().addAll(labelSecond);
+		hbox2.setAlignment(Pos.CENTER_LEFT);
+		
+		hbox.setAlignment(Pos.BOTTOM_LEFT);
+		hbox.getChildren().addAll(labelFirst, hbox2);
 
 		return hbox;
 	}
@@ -29,7 +33,7 @@ public class SuperSubStringHBox {
 
 		labelFirst.setText(firstString);
 		labelSecond.setText(secondString);
-		labelSecond.setFont(new Font("Areal", 7));
+		labelSecond.setFont(new Font("Areal", 8));
 
 		hbox.setAlignment(Pos.BOTTOM_LEFT);
 		hbox.getChildren().addAll(labelFirst, labelSecond, labelColon);
