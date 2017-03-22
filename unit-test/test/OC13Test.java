7b4a935a737af10ea79ef81e2dd6cd02da420cb4
Steffen <steffen_son@hotmail.com>
2017-03-22 13:31:14 +0100

    OC3 Test 3/3 virker, samt rettet tilhørene klasser

--- unit-test/test/OC13Test.java
+++ unit-test/test/OC13Test.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
+import exceptions.BoejningsMomentEjDefineretException;
 import exceptions.DimensionerendeKraftEjDefineretException;
 import exceptions.LaengdeEjDefineretException;
 import exceptions.erUnderFejlgraenseException;
@@ -21,42 +22,36 @@ import logic.PTECalculatorControllerImpl;
 public class OC13Test {
 
 	@Test
-	public void testkorrektudregningAfBoejningsMoment()	throws DimensionerendeKraftEjDefineretException, erUnderFejlgraenseException {
+	public void testkorrektudregningAfBoejningsMoment() throws DimensionerendeKraftEjDefineretException,
+			erUnderFejlgraenseException, LaengdeEjDefineretException, BoejningsMomentEjDefineretException {
 		PTECalculatorController calc = new PTECalculatorControllerImpl();
-		
-		calc.
-		
-		
-		Laengde l2 = new LaengdeImpl();
-		Dimensionerendekraft fdim = new DimensionerendekraftImpl();
-		BoejningsMoment mb = new BoejningsMomentImpl();
 
-		l2.angivLaengde(20);
-		fdim.setVaegt(60, Enhed.NEWTON);
-		mb.angivDimensionerendeKraft(fdim);
-		mb.angivLaengde(l2);
-		mb.beregnBoejningsMoment();
-		double mbtal = mb.getBoejningsMoment();
-		assertEquals(1200, mbtal, 0.001);
-		
+		calc.angivLaengde(1400);
+		calc.angivVaegt(495, Enhed.KG);
+		calc.beregnBoejningsMoment();
+
+		assertEquals(6802488, calc.getBoejningsMoment(), 0.001);
+
 	}
 
 	@Test(expected = DimensionerendeKraftEjDefineretException.class)
-	public void testDimensionerendeKraftNull() throws DimensionerendeKraftEjDefineretException, erUnderFejlgraenseException {
+	public void testDimensionerendeKraftNull()
+			throws DimensionerendeKraftEjDefineretException, erUnderFejlgraenseException {
 		PTECalculatorController calc = new PTECalculatorControllerImpl();
-		
+
 		calc.getDimensionerendekraft();
-		
+
 		fail("DimensionerendeKraftEjDefineretException ej indtruffet");
 	}
 
-	@Test (expected = LaengdeEjDefineretException.class)
-	public void testLaengdeNulltest() throws DimensionerendeKraftEjDefineretException, erUnderFejlgraenseException, LaengdeEjDefineretException {
+	@Test(expected = LaengdeEjDefineretException.class)
+	public void testLaengdeNulltest()
+			throws DimensionerendeKraftEjDefineretException, erUnderFejlgraenseException, LaengdeEjDefineretException {
 		PTECalculatorController calc = new PTECalculatorControllerImpl();
 
 		calc.angivVaegt(10, Enhed.KG);
 		calc.beregnBoejningsMoment();
-		
+
 		fail("LaengdeEjDefineretException ej indtruffet");
 	}
 
