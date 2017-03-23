cebb9db4c6ef02323ea7b36be6e82700b01b81a3
Laura <Laura@LauraPoyner.ceuherning.int>
2017-03-23 14:40:49 +0100

    Mellemregninger formateret til at have 5 decimaler efter komma - Tvaerkraft

--- src/logic/TvaerkraftImpl.java
+++ src/logic/TvaerkraftImpl.java
@@ -1,5 +1,7 @@
 package logic;
 
+import java.text.DecimalFormat;
+
 import exceptions.DimensionerendeKraftEjDefineretException;
 import exceptions.VinkelEjDefineretException;
 
@@ -8,6 +10,8 @@ public class TvaerkraftImpl implements Tvaerkraft {
 	private Dimensionerendekraft fdim = new DimensionerendekraftImpl();
 	private String mellemregning = null;
 	private double ftNewton = 0;
+	
+	DecimalFormat formatter = new DecimalFormat("#0.00000");
 	
 	@Override
 	public String getMellemregning() throws DimensionerendeKraftEjDefineretException, VinkelEjDefineretException {
@@ -37,15 +41,21 @@ public class TvaerkraftImpl implements Tvaerkraft {
 		double cosV = Math.cos(Math.toRadians(grader));
 		if (erMaaltTilLodret==false) {
 			ftNewton = cosV * fdimNewton;
+			String newtonText = formatter.format(ftNewton);
+			String fdimText = formatter.format(fdimNewton);
 		//	mellemregning = "FT = cos(vinkel) * FDim" + "\n" + ftNewton + " = cos(" + grader + ") * " + fdimNewton + "\n"; 
 		//	mellemregning = "FT\t	 =\t	 cos(vinkel)\t	 *	 FDim" + "\n" + ftNewton +"\t	"+" = cos(" + grader + ")\t	 * 	" + fdimNewton + "\n";
-			mellemregning = "cos(vinkel)\t	 *\t	FDim\t	=\t	FT	" + "\n" + "  cos(" + grader + ")\t	 *\t	" + fdimNewton + "\t	=\t" + ftNewton + "\n" ; 
+			//mellemregning = "cos(vinkel)\t	 *\t	FDim\t	=\t	FT	" + "\n" + "  cos(" + grader + ")\t	 *\t	" + fdimNewton + "\t	=\t" + ftNewton + "\n" ; 
+			mellemregning = "cos(vinkel)\t	 *\t	FDim\t	=\t	FT	" + "\n" + "  cos(" + grader + ")\t	 *\t	" + fdimText + "\t	=\t" + newtonText + "\n" ; 
 		}
 		
 		else {
 			ftNewton = sinV * fdimNewton;
+			String newtonText = formatter.format(ftNewton);
+			String fdimText = formatter.format(fdimNewton);
 		//	mellemregning = "FT = sin(vinkel) * FDim" + "\n" + ftNewton + " = sin(" + grader + ") * " + fdimNewton + "\n";
-			mellemregning = "sin(vinkel) * FDim" +"= FT" + "\n" + ftNewton + " = sin(" + grader + ") * " + fdimNewton + "\n"; 
+			// mellemregning = "sin(vinkel) * FDim" +"= FT" + "\n" + ftNewton + " = sin(" + grader + ") * " + fdimNewton + "\n"; 
+			mellemregning = "sin(vinkel) * FDim" +"= FT" + "\n" + newtonText + " = sin(" + grader + ") * " + fdimText + "\n"; 
 
 		}
 		
