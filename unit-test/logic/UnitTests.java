ae5dfd451bff3a2f0792089f42f54ee1268f50a5
eamv-awp <awp@eamv.dk>
2017-03-23 09:27:58 +0100

    Unit test suite opdateret.

--- unit-test/logic/UnitTests.java
+++ unit-test/logic/UnitTests.java
@@ -9,10 +9,12 @@ import test.BoejningsspaendingTest;
 @Suite.SuiteClasses({ 
 	BeregnSigmaN.class, 
 	BoejningsmomentTest.class, 
+	FlydespaendingTest.class,
 	ForskydningsSpaendingTest.class,
 	HalvProfilhoejdeTest.class, 
 	InertimomentTest.class, 
 	NormalkraftTest.class,
+	SikkerhedsfaktorTest.class,
 	BoejningsspaendingTest.class})
 
 public class UnitTests {
